1d1f011ab33fbcd00919691fba4dbbdd624bc7c5
Gleb <gkebkasyak@gmail.com>
2019-12-13 15:08:18 +0300

    Updates

---
+++ precache-manifest.a9634be52bd3b9189466b84632367b5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46d892ad871b0a2d403be4c2084a3bf",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "655a6eb9ef4ce251e663",
+    "url": "/tic-tac-toe/static/css/main.a529d7e7.chunk.css"
+  },
+  {
+    "revision": "c47a3364149a5ba110fb",
+    "url": "/tic-tac-toe/static/js/2.e2ded0bc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.e2ded0bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "655a6eb9ef4ce251e663",
+    "url": "/tic-tac-toe/static/js/main.d5ca0c79.chunk.js"
+  },
+  {
+    "revision": "3ee2ea92fe89de4adac5",
+    "url": "/tic-tac-toe/static/js/runtime-main.9fe9ee25.js"
+  }
+]);
